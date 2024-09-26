7b8cd30d4ed84af258e6c08631ac3b9646a8a774
n1kras <mariano.nicolaso@gmail.com>
2024-09-26 15:24:15 -0400

    desafio concluido

---
+++ src/Funcionario.java
@@ -0,0 +1,30 @@
+public class Funcionario {
+    private String nome;
+    private double salarioBase;
+
+    public Funcionario(String nome, double salarioBase) {
+        this.nome = nome;
+        this.salarioBase = salarioBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public double calcularSalario() {
+        return salarioBase;
+    }
+
+}
---
+++ src/Gerente.java
@@ -0,0 +1,22 @@
+public class Gerente extends Funcionario {
+    private double bonusSalarial;
+
+    public Gerente(String nome, double salarioBase) {
+        super(nome, salarioBase);
+    }
+
+    public double getBonusSalarial() {
+        return bonusSalarial;
+    }
+
+    public void setBonusSalarial(double bonusSalarial) {
+        this.bonusSalarial = bonusSalarial;
+    }
+
+    @Override
+    public double calcularSalario() {
+        double salarioTotal;
+        salarioTotal = this.getSalarioBase()+bonusSalarial;
+        return salarioTotal;
+    }
+}
